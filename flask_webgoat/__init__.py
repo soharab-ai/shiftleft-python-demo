51a6f2715093dac5cf84461d2920d9e1d07c52d4
soharab-ai <soharab.infracloud@qwiet.ai>
2025-01-16 11:00:24 +0530

    Fixing flask_webgoat/__init__.py for finding 14

--- flask_webgoat/__init__.py
+++ flask_webgoat/__init__.py
@@ -9,12 +9,11 @@ DB_FILENAME = "database.db"
 
 def query_db(query, args=(), one=False, commit=False):
     with sqlite3.connect(DB_FILENAME) as conn:
-        # vulnerability: Sensitive Data Exposure
-        conn.set_trace_callback(print)
         cur = conn.cursor().execute(query, args)
         if commit:
             conn.commit()
         return cur.fetchone() if one else cur.fetchall()
+
 
 
 def create_app():
